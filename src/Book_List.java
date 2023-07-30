29f429068cdc824e394bc8af46b1a74b51f4d30b
大川原誠人 <ookawaramasato@ookawaramasatonoMacBook-Pro.local>
2023-07-30 20:53:57 +0900

    第5回

--- src/Book_List.java
+++ src/Book_List.java
@@ -8,7 +8,7 @@ public class Book_List {
         List<String> result = new ArrayList<>();
 
         for (String name : names) {
-            if (name.contains("ライアー")) {
+            if (name.contains("ノーゲーム")) {
                 result.add(name);
             }
         }
