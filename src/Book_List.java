168bff3460b44781ba7dda4760cc2ac935989dc1
大川原誠人 <ookawaramasato@ookawaramasatonoMacBook-Pro.local>
2023-07-30 20:37:37 +0900

    第5回

---
+++ src/Book_List.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book_List {
+    public static void main(String[] age) {
+
+        List<String> names = List.of("ノーゲーム・ノーライフ1", "ノーゲーム・ノーライフ2", "ライアー・ライアー1", "ライアー・ライアー2");
+        List<String> result = new ArrayList<>();
+
+        for (String name : names) {
+            if (name.contains("ノーゲーム・ノーライフ")) {
+                result.add(name);
+            }
+        }
+        System.out.println(result);
+    }
+}
